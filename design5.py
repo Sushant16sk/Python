960ffacdd7e8c595d381380f59970ba58a1fbba3
Sushant <82380408+Sushant16sk@users.noreply.github.com>
2022-09-19 00:11:26 +0530

    Add files via upload

---
+++ design5.py
@@ -0,0 +1,12 @@
+for i in range(6):
+    for j in range(6-i):
+        print(' ',end='')
+    for k in range(2*i-1):
+        print('*',end='')
+    print()
+for i in range(6,0,-1):
+    for j in range(6-i):
+        print(' ',end='')
+    for k in range(2*i-1):
+        print('*',end='')
+    print()
---
+++ design7.py
@@ -0,0 +1,6 @@
+for i in range(7):
+    for j in range(7-i):
+        print(' ',end='')
+    for k in range(1,i+1):
+        print(k,end='')
+    print('\n')
